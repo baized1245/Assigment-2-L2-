d5a70ade4641fde0af4fc2da5b50c24665f3102d
unknown <you@example.com>
2024-05-21 00:01:20 +0600

    Create mongoose schema for product

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,13 +1,16 @@
+// Define the Variant type
 export type TVariant = {
   type: string;
   value: string;
 };
 
+// Define the Inventory type
 export type TInventory = {
   quantity: number;
   inStock: boolean;
 };
 
+// Define the Product type
 export type TProduct = {
   name: string;
   description: string;
